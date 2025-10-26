6af2362a537e8f67c18dbb5f2c0a468b3b467ef7
y0113575 <henriette.watolla@tu-braunschweig.de>
2025-10-26 19:29:26 +0100

    add 00_foobar first example

--- sheets/01_cpsat/exercises/00_foobar/solution_python.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_python.py
@@ -5,9 +5,13 @@ def solve(instance: Instance) -> Solution:
     """
     Implement your solver for the problem here!
     """
+
     numbers = instance.numbers
+
+    numbers.sort()  #smallest element is first, biggest element is last
+
     return Solution(
         number_a=numbers[0],
-        number_b=numbers[-1],
+        number_b=numbers[-1], #access last element
         distance=abs(numbers[0] - numbers[-1]),
     )
