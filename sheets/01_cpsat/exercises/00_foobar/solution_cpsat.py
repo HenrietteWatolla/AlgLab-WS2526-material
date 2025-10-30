ac217f39f7e099c0b45b685c6fa1a734a11f723d
y0113575 <henriette.watolla@tu-braunschweig.de>
2025-10-30 11:54:46 +0100

    finish 00_foobar

--- sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
@@ -7,9 +7,18 @@ def solve(instance: Instance) -> Solution:
     Implement your solver for the problem here!
     """
     numbers = instance.numbers
+    domain = cp_model.Domain.from_values(numbers)
+
     model = cp_model.CpModel()
+    x = model.new_int_var_from_domain(domain, "x")
+    y = model.new_int_var_from_domain(domain, "y")
+    distance = x - y
+    model.maximize(distance)
+    solver = cp_model.CpSolver()
+    status = solver.solve(model)
+
     return Solution(
-        number_a=numbers[0],
-        number_b=numbers[-1],
-        distance=abs(numbers[0] - numbers[-1]),
+        number_a = solver.value(x),
+        number_b = solver.value(y),
+        distance = solver.value(distance)
     )
