159a3dfac109ddf2f96b5840cca4a1343cdd9518
y0113575 <henriette.watolla@tu-braunschweig.de>
2025-12-08 16:27:39 +0100

    work on flow

--- sheets/04_mip/exercises/02_flow/solution.py
+++ sheets/04_mip/exercises/02_flow/solution.py
@@ -20,7 +20,50 @@ class MiningRoutingSolver:
             instance.budget,
         )
         # TODO: Implement me!
+        self._model = gp.Model()
 
+        # get parameters
+        w_tunnels = []
+        for t in instance.tunnels:
+            w_tunnels.append([t.source, t.target, t.throughput_per_hour, t.reinforcement_costs])
+        w_mines = []
+        for m in instance.mines.values():
+            w_mines.append([m.location, m.ore_per_hour])
+
+        #print("sources ", w_tunnels, "\n")
+        #print("mines ", w_mines)
+
+        # create variables, consider both tunnel directions
+        # number of ores transported along this direction
+        flow_variables = {}
+        # tunnel is used or not
+        usage_variables = {}
+        for t in w_tunnels:
+            flow_variables[(t[0], t[1])] = self._model.addVar(vtype = GRB.INTEGER, name = f"flow_{t[0]}_{t[1]}", lb = 0)
+            flow_variables[(t[1], t[0])] = self._model.addVar(vtype = GRB.INTEGER, name = f"flow_{t[1]}_{t[0]}", lb = 0)
+            usage_variables[(t[0], t[1])] = self._model.addVar(vtype = GRB.BINARY, name = f"tunnel_{t[0]}_{t[1]}")
+            usage_variables[(t[1], t[0])] = self._model.addVar(vtype = GRB.BINARY, name = f"tunnel_{t[1]}_{t[0]}")
+
+        # add constraints
+        # only one direction can be used at once
+        for source, target, throughput, costs in w_tunnels:
+            self._model.addConstr(
+                (usage_variables[(source, target)] + usage_variables[(target, source)] <= 1)
+            )
+            # dont exceed maximal throughput of the tunnels
+            self._model.addConstr(
+                (flow_variables[(source, target)] <= throughput * usage_variables[(source, target)])
+            )
+            self._model.addConstr(
+                (flow_variables[(target, source)] <= throughput * usage_variables[(target, source)])
+            )
+        # budget constraint
+        self._model.addConstr(
+                (gp.quicksum(costs * (usage_variables[(source, target)] + usage_variables[(target, source)])
+                    for source, target, throughput, costs in w_tunnels) <= instance.budget)
+            )
+
+        # obejctive function
 
     def solve(self) -> Solution:
         """
