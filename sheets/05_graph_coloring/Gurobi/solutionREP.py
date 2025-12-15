71f09d099d0b197f1c41e6bfd5c60aaa307953c6
y0113575 <henriette.watolla@tu-braunschweig.de>
2025-12-15 15:41:45 +0100

    add REP for gurobi

---
+++ sheets/05_graph_coloring/Gurobi/solutionREP.py
@@ -0,0 +1,119 @@
+import gurobipy as gp
+import networkx as nx
+from gurobipy import GRB
+
+from instances.instances import Instances
+from heuristics.heuristics import Heuristics
+
+class GurobiREP:
+    def solve_coloring_REP_gurobi(G: nx.Graph, time_limit = 60) -> int:
+
+        vertices = sorted(G.nodes) # sorted to ensure that smallest index node is used as representant of class
+        edges = list(G.edges)
+
+        model = gp.Model()
+
+        model.Params.TimeLimit = time_limit
+
+        # create variables
+        # nodes v, w are colored with the same color (1) --> w is representant of v
+        same_color = {}
+        for v in vertices:
+            for w in vertices:
+                # ordering to choose smallest possible representant
+                if w <= v and w not in G.neighbors(v):
+                    same_color[(v, w)] = model.addVar(vtype = GRB.BINARY, name = f"same_color{v}_{w}")
+        
+        # add constraints
+        # each vertex must choose exactly one representative
+        for v in vertices:
+            model.addConstr(gp.quicksum(same_color[(v, w)]
+                                for w in vertices
+                                if (v, w) in same_color)
+                                == 1
+                            )
+
+        # two adjacent nodes can't choose the same representant
+        # + if v selects w as its representative, then w must itself be marked as representative
+
+        for (v, w) in same_color:
+            model.addConstr(
+                same_color[(v, w)] <= same_color[(w, w)]
+            )
+
+        for (u, v) in edges:
+            for w in vertices:
+                # w must be a valid representative for both u and v
+                if (((u, w) in same_color) and ((v, w) in same_color)):
+                    model.addConstr(
+                        same_color[(u, w)] + same_color[(v, w)] <= same_color[(w, w)]
+                    )
+
+        # obejctive function --> minimizing used colors, this is equal to minimize nodes that are representatives of themselves
+        model.setObjective(gp.quicksum(same_color[(v, v)] for v in vertices), GRB.MINIMIZE)
+
+        # solve the model
+        model.optimize()
+
+        # extract solution
+
+        solution = {
+            "status": model.Status,
+            "objective": None,
+            "coloring": None,
+            "runtime": model.Runtime
+        }
+        
+        coloring = {}
+
+        if model.SolCount > 0:
+
+            # get node classes
+            representatives = [
+                w for w in vertices
+                if same_color[(w, w)].X > 0.5
+            ]
+
+            # one color index per representant
+            rep_to_color = {rep: i for i, rep in enumerate(representatives)}
+
+            # assign colors to vertices
+            for v in vertices:
+                for w in vertices:
+                    if (v, w) in same_color and same_color[(v, w)].X > 0.5:
+                        coloring[v] = rep_to_color[w]
+                        break
+
+        solution["objective"] = sum(1 for v in vertices if same_color[(v, v)].X > 0.5)
+        solution["coloring"] = coloring
+
+        print(solution["objective"], "\n")
+        print(solution)
+
+        return solution["objective"]
+
+
+G = nx.complete_graph(5)
+res = GurobiREP.solve_coloring_REP_gurobi(G)
+print(res)
+
+G = nx.path_graph(10)
+print(GurobiREP.solve_coloring_REP_gurobi(G))
+
+G = nx.cycle_graph(5)
+print(GurobiREP.solve_coloring_REP_gurobi(G))
+
+G = nx.complete_bipartite_graph(5,5)
+print(GurobiREP.solve_coloring_REP_gurobi(G))
+
+# too difficult
+#G = nx.kneser_graph(14, 4)
+#print(GurobiASS.solve_coloring_ASS_gurobi(G, 8))
+#G = nx.kneser_graph(13, 4)
+#print(GurobiASS.solve_coloring_ASS_gurobi(G, 7))
+
+G = nx.kneser_graph(5, 2)
+print(GurobiREP.solve_coloring_REP_gurobi(G))
+
+#G = nx.kneser_graph(13, 2)
+#print(GurobiREP.solve_coloring_REP_gurobi(G))
